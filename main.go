4f7ea2c5b63843e6a86d48af1fd58865c7fde5ce
viktorbo <borisoof13@yandex.ru>
2022-05-12 15:43:08 +0300

    Added handling some URLs

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func HomePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hmm, mb it's easy")
+}
+
+func ContactsPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Contacts")
+}
+
+func HandleRequest() {
+	http.HandleFunc("/", HomePage)
+	http.HandleFunc("/contacts", ContactsPage)
+	http.ListenAndServe(":8080", nil)
+}
 
 func main() {
-	fmt.Println("JOJA")
+	HandleRequest()
 }
